4c1773bc257480bca257f5967735bd85dc1d7aa5
AmberRoseA <>
2024-02-07 02:22:26 +0000

    Created start game function with initial messages to player on how to play also enter name input added and ready to start input added

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import random
+["♠", "♥", "♦", "♣"]
 
 """
 Deck of cards and values
@@ -20,7 +21,30 @@ Check the winner
 """
 
 # Game Welcome/ start/ rules of the game 
-["♠", "♥", "♦", "♣"]
+
+def start_game():
+    """
+    Start Game and explains the rules
+    """
+    print("This is Blackjack !!!")
+    print("The goal of the game is to get as close to 21 as possible without going over")
+    print("You will be dealt two cards, and you can choose to hit or stick.")
+    print("If you hit, you will be dealt another card. If you stick, your turn will end.")
+    print("If you go over 21, you lose. If you get 21, you win. If you get closer to 21 than the dealer, you win. If the dealer gets closer to 21 than you, you lose. If you get 21 and the dealer gets 21, it's a tie.")
+
+    player_name = input("Enter players name: \n")
+    ready_to_start = input(f"Are you ready to start, {player_name} ?(y/n) \n")
+
+    if ready_to_start == "y":
+        print("Let's start the game!")
+    elif ready_to_start == "n":
+        print("Maybe next time, Goodbye")
+    else:
+        print("Invalid input. please enter 'y' or 'n'.")
+
+start_game()                
+
+
 
 
 
