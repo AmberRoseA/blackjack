78b6ebce22450bd06a473bcaa938e5e9596e6650
AmberRoseA <>
2024-02-09 01:44:32 +0000

    Added def inside class hand, gave bumber values to J Q K and A

--- run.py
+++ run.py
@@ -27,70 +27,64 @@ class Deck:
         else:
             print("No more cards in the deck.")
             return None                 
-    
-    """
-    cards = []
-    suits = ["♠", "♥", "♦", "♣"]
-    faces = ["A", "2", "3", "4", "5", "6", "7", "8", "9", 
-                "10", "J", "Q", "K"]
-    """            
-
-    for suit in suits:
-        for face in faces:
-            cards.append([suit, face])
-
-    def shuffle():
-        random.shuffle(cards)
-
-    def deal(number):
-        dealt_cards = []
-        for x in range(number):
-            card = cards.pop()
-            dealt_cards.append(card)
-        return dealt_cards
-
-    shuffle()
-    print(cards)
-
-"""
-Dealing the cards
-"""
 
 
-"""
-Calculate the totalof each hand
-"""
+class Hand:
+    def __init__(self, name):
+        self.cards = []
+        self.name = name
+
+    def add_card(self, card):
+        sefl.cards.append(card)
+
+    def get_value(self):
+        value = 0
+        num_aces = 0
+        for card in self.cards:
+            if card.face in ["J", "Q", "K"]:
+                value += 10
+            elif card.face == "A":
+                value += 11
+                num_aces += 1
+            return value                
 
 
-"""
-Check the winner
-"""
+
 
 # Game Welcome/ start/ rules of the game 
-"""
-def start_game():
-   
-    Start Game and explains the rules
-   
-    print("This is Blackjack !!!")
-    print("The goal of the game is to get as close to 21 as possible without going over")
-    print("You will be dealt two cards, and you can choose to hit or stick.")
-    print("If you hit, you will be dealt another card. If you stick, your turn will end.")
-    print("If you go over 21, you lose. If you get 21, you win. If you get closer to 21 than the dealer, you win. If the dealer gets closer to 21 than you, you lose. If you get 21 and the dealer gets 21, it's a tie.")
+class TheGame:
+    def __init__(self):
+        delf.deck = Deck()
 
-    player_name = input("Enter players name: \n")
-    ready_to_start = input(f"Are you ready to start, {player_name} ?(y/n) \n")
+    def start_game():
+        while True:
+            print("This is Blackjack!")
+            print("The goal of the game is to get as close to 21 as possible without going over")
+            print("You will be dealt two cards, and you can choose to Hit or Stick.")
 
-    if ready_to_start == "y":
-        print("Let's start the game!")
-    elif ready_to_start == "n":
-        print("Maybe next time, Goodbye")
-    else:
-        print("Invalid input. please enter 'y' or 'n'.")
+            player_name = input("Enter players name: \n")
+            ready_to_start = input(f"Are you ready to start, {player_name} ?(y/n) \n")
 
-start_game()                
+            if ready_to_start == "y":
+                print("Let's start the game!")
+                return TheGame
+            elif ready_to_start == "n":
+                print("Maybe next time, Goodbye")
+            else:
+                print("Invalid input. please enter 'y' or 'n'.")
 
-"""
+            player_hand =
+            dealer_hand =
+
+            # Deal first two card
+            # Players turn
+            # Dealers turn
+            # Check winner
+            # Ask player to play again     
+
+    start_game()                
+
+
 
 
 
